6ca3eff4f85696a9184787c0418dc5b4e3676fa8
Rithik-02 <rithirithi246863@gmail.com>
2024-03-23 13:05:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+/** @format */
+
+import logo from "./logo.svg";
+import "./App.css";
+import ScrollSpy from "react-ui-scrollspy";
+import Scrollspy from "react-scrollspy";
+import TEST from "./test";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   // const test = [0, 1, 2, 3, 4, 5, 6, 7,];
+
+   // const sampleArray = [
+   //    "about",
+   //    "contact",
+   //    "tools",
+   //    "experiences",
+   //    "home",
+   //    "skills",
+   //    "projects",
+   //    "services",
+   //    "resume",
+   //    "volunteer_works",
+   //    "certifications",
+   //    "hobbies",
+   //    "languages",
+   //    "how_can_i_contribute",
+   // ];
+   // console.log(sampleArray[9]);
+
+   // const object = {
+   //    name: "naveen",
+   //    age: 19,
+   //    height: 5,
+   //    school: "St marys",
+   // };
+   // console.log(Object.name);
+
+   // const arrayObject = [
+   //    {
+   //       name: "naveen",
+   //       age: 19,
+   //       height: 5,
+   //       school: "St marys",
+   //    },
+   //    {
+   //       name: "gayle",
+   //       age: 19,
+   //       height: 5,
+   //       school: "St marys",
+   //    },
+   //    {
+   //       name: "dinesh",
+   //       age: 19,
+   //       height: 5,
+   //       school: "St marys",
+   //    },
+   // ];
+   // console.log(arrayObject[2].name);
+
+   //spread operator
+   // const array1 = [1, 2, 3];
+   // const array2 = [...array1, 9, 8, 7];
+
+   // console.log(array2);
+
+   //Array destructuring
+
+   //Normal function
+   // function myFunc() {
+   //    console.log("NORMAL FUNC");
+   // }
+
+   // myFunc();
+
+   //Arrow function
+   // const arrowFunction = () => console.log("ARROW FUNC");
+
+   arrowFunction();
+
+   return <div></div>;
 }
 
 export default App;
+
+// Array
+// Object
+// Dot operator
+// Array Object
+// Spreas operator
---
+++ src/test.jsx
@@ -0,0 +1,14 @@
+/** @format */
+
+import React from "react";
+
+export default function test() {
+   return (
+      <div style={{ position: "fixed" }}>
+         <p data-to-scrollspy-id="first">Section 1</p>
+         <p data-to-scrollspy-id="second">Section 2</p>
+         <p data-to-scrollspy-id="third">Section 3</p>
+         <p data-to-scrollspy-id="fourth">Section 4</p>
+      </div>
+   );
+}
